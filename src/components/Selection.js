0c166a48b2e2b70e3322caba07bcf4dda08172f7
Yash Prakash Mishra <34412462+xcalibre1@users.noreply.github.com>
2021-02-04 02:51:25 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,12 @@
-
+import React {useState} from 'react';
+const Selection=(props)=>{
+  const [background,backroundSet]=useState({background:''})
+  const updateSelectionStyle=(background)=>{
+      backgroundSet(nextBackground)
+  }
+  
+  col=updateSelectionStyle({background:''})
+  <div className="fix-box" style={background} onClick={()=>(props.applyColor(updateSelectionStyle))}>
+    <h2 className="subheading">Selection</h2>  
+  </div>
+}
